8b8a04bbcbbdfdcdcdbf2bc83c7430fb7610b8c0
samjhecht <samjhecht@gmail.com>
2013-11-23 21:44:23 -0800

    adding unicorns

--- unicorns/views.py
+++ unicorns/views.py
@@ -2,5 +2,36 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def hello(request):
-    return HttpResponse("Hello world")
+def homepage(request):
+
+		html = """<html><head></head><body>
+							<pre>
+
+							  ________________________   \
+							 &lt; Unicorns are the best! &gt;   \
+							  ------------------------     \\
+							                     \          \\
+							                      \          &gt;\/7
+							                             _.-(6'  \
+							                            (=___._/` \
+							                                 )  \ |
+							                                /   / |
+							                               /    &gt; /
+							                              j    &lt; _\
+							                          _.-' :      ``.
+							                          \ r=._\        `.
+							                         &lt;`\\_  \         .`-.
+							                          \ r-7  `-. ._  ' .  `\
+							                           \`,      `-.`7  7)   )
+							                            \/         \|  \'  / `-._
+							                                      ||    .'
+							                                       \\  (
+							                                        &gt;\  &gt;
+							                                     ,.-' &gt;.'
+							                                    &lt;.'_.''
+							                                      &lt;'
+							</pre>
+							
+							</body></html>"""
+
+    return HttpResponse(html)
--- unicornssay/urls.py
+++ unicornssay/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from unicorns.views import hello
+from unicorns.views import homepage
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'unicornssay.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^hello/$', hello),
+    url(r'^$', homepage),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
