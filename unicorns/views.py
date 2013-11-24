209ac2421b711b99c9abcb2394e41bc3a7c567f4
samjhecht <samjhecht@gmail.com>
2013-11-23 21:50:27 -0800

    reformat

--- unicorns/views.py
+++ unicorns/views.py
@@ -4,34 +4,37 @@ from django.http import HttpResponse
 
 def homepage(request):
 
-		html = """<html><head></head><body>
-							<pre>
-
-							  ________________________   \
-							 &lt; Unicorns are the best! &gt;   \
-							  ------------------------     \\
-							                     \          \\
-							                      \          &gt;\/7
-							                             _.-(6'  \
-							                            (=___._/` \
-							                                 )  \ |
-							                                /   / |
-							                               /    &gt; /
-							                              j    &lt; _\
-							                          _.-' :      ``.
-							                          \ r=._\        `.
-							                         &lt;`\\_  \         .`-.
-							                          \ r-7  `-. ._  ' .  `\
-							                           \`,      `-.`7  7)   )
-							                            \/         \|  \'  / `-._
-							                                      ||    .'
-							                                       \\  (
-							                                        &gt;\  &gt;
-							                                     ,.-' &gt;.'
-							                                    &lt;.'_.''
-							                                      &lt;'
-							</pre>
+		html = """<html>
+								<head></head>
+								<body>
+									
+									<pre>
+										  ________________________   \
+										 &lt; Unicorns are the best! &gt;   \
+										  ------------------------     \\
+										                     \          \\
+										                      \          &gt;\/7
+										                             _.-(6'  \
+										                            (=___._/` \
+										                                 )  \ |
+										                                /   / |
+										                               /    &gt; /
+										                              j    &lt; _\
+										                          _.-' :      ``.
+										                          \ r=._\        `.
+										                         &lt;`\\_  \         .`-.
+										                          \ r-7  `-. ._  ' .  `\
+										                           \`,      `-.`7  7)   )
+										                            \/         \|  \'  / `-._
+										                                      ||    .'
+										                                       \\  (
+										                                        &gt;\  &gt;
+										                                     ,.-' &gt;.'
+										                                    &lt;.'_.''
+										                                      &lt;'
+									</pre>
 							
-							</body></html>"""
+								</body>
+							</html>"""
 
     return HttpResponse(html)
