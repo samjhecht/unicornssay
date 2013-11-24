0dec13300947630ffbe499d4fdab32a1e3733e8b
samjhecht <samjhecht@gmail.com>
2013-11-23 21:27:16 -0800

    adding a view

--- unicornssay/urls.py
+++ unicornssay/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from mysite.views import hello
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'unicornssay.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^hello/$', hello),
 
     url(r'^admin/', include(admin.site.urls)),
 )
